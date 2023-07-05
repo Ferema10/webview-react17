5066e98c9b1b207af8f096226ded2e043191695b
Ferema10 <mendoza1im15@gmail.com>
2023-07-05 13:20:10 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  const sendMessageNative = () => {
+    //window.ReactNativeWebView.postMessage("Open Camera Native");
+    //find a postMessage to talk to hoster
+    let postMessage = window.parent.postMessage;
+    if (window.ReactNativeWebView) {
+      postMessage = window.ReactNativeWebView.postMessage;
+    }
+    postMessage('Open Camera Native');
+    console.log('Comando enviado')
+  };
+
+  return (
+    <>
+      <h1>Webview + Native</h1>
+      <div className="card">
+        <button onClick={() => sendMessageNative()}>Abrir camara</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
